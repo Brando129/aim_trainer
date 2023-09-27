e1f109919f066c00ad9e0207571edb8f6797cc75
Brandon Colangelo <brandoncancode@gmail.com>
2023-09-27 11:03:56 -0400

    Collision detection.

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ class Target:
         self.size = 0
         self.grow = True
 
+    # Updates the target growth rate
     def update(self):
         if self.size + self.GROWTH_RATE >= self.MAX_SIZE:
             self.grow = False
@@ -46,6 +47,11 @@ class Target:
         pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
         pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
 
+    # Detects collision
+    def collide(self, x, y):
+        dis = math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2)
+        return dis <= self.size
+
 # Draws on the main screen
 def draw(win, targets):
     win.fill(BG_COLOR)
@@ -61,11 +67,18 @@ def main():
     targets = []
     clock = pygame.time.Clock()
 
+    target_pressed = 0
+    clicks = 0
+    misses = 0
+    start_time = time.time()
+
     pygame.time.set_timer(TARGET_EVENT, TARGET_INCREMENT)
 
     # Event Loop
     while run:
         clock.tick(60)
+        click = False
+        mouse_pos = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -78,11 +91,20 @@ def main():
                 target = Target(x, y)
                 targets.append(target)
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+                clicks += 1
+
         for target in targets:
             target.update()
 
             if target.size <= 0:
                 targets.remove(targets)
+                misses += 1
+
+            if click and target.collide(*mouse_pos):
+                targets.remove(target)
+                target_pressed += 1
 
         draw(WIN, targets)
 
